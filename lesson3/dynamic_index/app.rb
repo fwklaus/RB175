198588ba98d91c6a3787ee331efe0a5b2569df48
Forrest Klausmeier <fwklausmeier@gmail.com>
2023-12-04 01:29:25 -0700

    Final Commit

--- lesson3/dynamic_index/app.rb
+++ lesson3/dynamic_index/app.rb
@@ -2,13 +2,7 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
-# Sinatra serves any file placed in a public directory that lives in the same directory as the Sinatra app
-
-# You can use the file extension in the url to serve it directly
-
-# The first route that matches a request is invoked
 get "/img" do
-  # image in the public directory
   "<img src='1.jpg'/>"
 end
 
@@ -41,12 +35,6 @@ get "/abcs" do
   @alphabet = ("A".."Z").to_a
   erb :abcs
 end
-
-# a named parameter (ie :sort_order) is accessed via the block parameter
-    # can also be accessed via the params hash - ie params['sort_order']
-# the named parameter does not name a file
-    # can almost think of it as a case statement
-    # the symbol names the condition we want to test
 
 get "/:sort_order" do |n|
   @contents = Dir.children("public")
